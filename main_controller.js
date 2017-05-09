3c9d41a416379eabf182f342bb6af9428339dcb1
Maria <Maria@home.at>
2017-05-09 23:37:29 +0300

    переделала

--- main_controller.js
+++ main_controller.js
@@ -1,4 +1,18 @@
-angular.module('app', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap'])
+angular.module('app', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap', 'ngRoute'])
+
+.run(function($rootScope) {
+    $rootScope.products = [
+      { name: 'Nirvana Super Shisha - Berry Blast', date: '05/01/17', number: 1, price: 1290 },
+      { name: 'Starbuzz Exotic - Pirates Cave', date: '12/02/17', number: 2, price: 1020 },
+      { name: 'Argelini - Red Fruit', date: '04/03/17', number: 4, price: 1050 },
+      { name: 'Starbuzz Exotic - Blue Mist', date: '30/03/17', number: 3, price: 1150 }
+    ];
+})
+
+  .controller('firstOne', function($scope, $rootScope) {
+   $scope.products = $rootScope.products;
+  })
+
   .filter('sumPrice', function () {
     return function (collection, column) {
       var total = 0;
@@ -10,14 +24,30 @@ angular.module('app', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap'])
       return total;
     };
   })
-  .controller('mainController', function($scope, $modal) {
-    $scope.products = [
-      { name: 'Nirvana Super Shisha - Berry Blast', date: '05/01/17', number: 1, price: 1290 },
-      { name: 'Starbuzz Exotic - Pirates Cave', date: '12/02/17', number: 2, price: 1020 },
-      { name: 'Argelini - Red Fruit', date: '04/03/17', number: 4, price: 1050 },
-      { name: 'Starbuzz Exotic - Blue Mist', date: '30/03/17', number: 3, price: 1150 }
-    ];
 
+   .config( ['$routeProvider',
+      function( $routeProvider ){ 
+        $routeProvider
+          .when( "/", {
+              templateUrl: "pages/main.html",
+              controller: 'firstOne'
+            }
+          )
+           .when( "/modal", {
+              templateUrl: "pages/modal.html",
+              controller: 'secondOne'
+            }
+          )
+          .otherwise( { redirectTo: '/' } 
+          );
+      }]
+    )
+   .config(['$locationProvider', function($locationProvider) {
+      $locationProvider.hashPrefix('');
+    }])
+
+
+    .controller('secondOne', function($scope, $rootScope) {
     $scope.refer = [
         {name:'Nirvana Super',price:'1500'},
         {name:'Nirvana Light',price:'1200'},
@@ -33,7 +63,7 @@ angular.module('app', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap'])
       $scope.name = _.find($scope.refer, function(item) { return item.name === $scope.addname});
     };
     $scope.add = function() {
-      $scope.products.push({
+      $rootScope.products.push({
         name: $scope.addname,
         price: $scope.name.price,
         date: $scope.selectedDate
@@ -46,15 +76,4 @@ angular.module('app', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap'])
 
      $scope.selectedDate= '';
 
-
-    var myModal = $modal({scope: $scope, templateURL: `modal.html`, show: false, selector: '.modal'});
-    $scope.showModal = function() {
-      myModal.$promise.then(myModal.show);
-    };
   })
-  .directive('modal', function() {
-    return {
-      templateUrl: 'modal.html'
-    }
-  });
-  
